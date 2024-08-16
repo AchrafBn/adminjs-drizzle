1a371bda009a9fe393e80ccb16f881c9c5f86bfd
Achraf <achraf.benaomar@gmail.com>
2024-08-16 15:46:37 +0100

    update main file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import express from 'express';
 import AdminJS from 'adminjs';
 import { buildAuthenticatedRouter } from '@adminjs/express';
+import { Database, Resource } from 'adminjs-drizzle/pg';
 
+import * as usersTable from './db/schema/user.js';
 import provider from './admin/auth-provider.js';
 import options from './admin/options.js';
+import { db } from './db/drizzle.js';
 
 const port = process.env.PORT || 3000;
+AdminJS.registerAdapter({ Database, Resource });
 
 const start = async () => {
   const app = express();
 
-  const admin = new AdminJS(options);
-
+  const admin = new AdminJS({
+    resources: [
+      { table: usersTable, db },
+    ],
+    rootPath: '/admin',
+  });
   if (process.env.NODE_ENV === 'production') {
     await admin.initialize();
   } else {
--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -9,4 +9,5 @@ const pool = new Pool({
 
 export const db = drizzle(pool, {
   schema,
+  logger: true,
 });
